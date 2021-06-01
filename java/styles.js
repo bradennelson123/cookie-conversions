443cb908051745765530fd8cad39d8f7d11e1d15
Braden Nelson <connornel123@outlook.com>
2021-06-01 14:51:36 -0700

    add temp and gallons converter to js

---
+++ java/styles.js
@@ -0,0 +1,10 @@
+
+// fahrenheit to celsius converter//
+const fahrenheit = (prompt ("enter number"));
+const celsius = (fahrenheit - 32) * .55556
+alert (celsius);
+
+// gallons to liters converter //
+const gallons =(prompt("enter number"));
+const liters = (gallons * 3.78)
+alert (liters);
