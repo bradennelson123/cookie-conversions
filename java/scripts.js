96805d269960f898d1e3024e19f86b5faf47f78e
Braden Nelson <connornel123@outlook.com>
2021-06-02 12:34:11 -0700

    add more conversions

--- java/scripts.js
+++ java/scripts.js
@@ -1,14 +1,3 @@
-
-// fahrenheit to celsius converter//
-const fahrenheit = (prompt ("enter number"));
-const celsius = (fahrenheit - 32) * .55556
-alert (celsius);
-
-// gallons to liters converter //
-const gallons =(prompt("enter number"));
-const liters = (gallons * 3.78)
-alert (liters);
-
 // addition calculator function //
 function add(number1, number2) {
   return number1 + number2;
@@ -16,8 +5,8 @@ function add(number1, number2) {
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-
-alert(add(number1, number2))
+const result = add(number1,number2);
+alert(result);
 
 // Subtraction calculator function //
 function subtract(number1,number2) {
