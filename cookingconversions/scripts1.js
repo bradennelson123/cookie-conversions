e6f0e3b8e4ed64444c1cf59d84e335c6fd2e5c10
Braden Nelson <connornel123@outlook.com>
2021-06-03 07:52:01 -0700

    add user interface logic to calculator

---
+++ cookingconversions/scripts1.js
@@ -0,0 +1,13 @@
+// temperature converter//
+const celsius = (prompt("enter degrees celsius"));
+
+const tempF = celsius * (9/5) +32;
+alert (tempF);
+
+const litres = (prompt("enter number of litres"));
+const gallons = litres * .2641
+alert (gallons);
+
+const ounces = (prompt("enter ounces"));
+const pounds = ounces *16
+alert (pounds);
